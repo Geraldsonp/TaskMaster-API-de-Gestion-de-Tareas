a2740319d7b911989656341132c22f652d4df483
Geraldson <Geraldson.a.p@gmail.com>
2023-04-10 16:21:18 -0400

    Adds Unit tests

--- Tests/UnitTests/TaskMaster.Api.UTest/TaskControllerUnitTest.cs
+++ Tests/UnitTests/TaskMaster.Api.UTest/TaskControllerUnitTest.cs
@@ -1,9 +1,14 @@
+using System.Net;
 using System.Reflection;
 using AutoMapper;
 using Moq;
+using TaskMaster.Api.Contracts;
 using TaskMaster.Api.Controllers;
 using TaskMaster.Application.MappingConfigs;
 using TaskMaster.Application.TaskEntity;
+using TaskMaster.Application.TaskEntity.Dtos;
+using TaskMaster.Domain.Enums;
+using TaskMaster.Domain.ValueObjects;
 
 namespace TaskMaster.Api.UTests;
 
@@ -12,6 +17,21 @@ public class TaskControllerUnitTest
     private Mock<ITaskEntityService> _taskService;
     private TaskController? _taskController;
     private IMapper _mapper;
+
+    private static IEnumerable<Object[]> goodData = new[]
+    {
+	    new object[]
+	    {
+		    new TaskCreateDto()
+		    {
+			    Description = "Test",
+			    Title = "Test",
+			    Priority = Priority.High,
+			    TicketType = TicketType.Bug
+		    },
+		    HttpStatusCode.OK
+	    }
+    };
 
     public TaskControllerUnitTest()
     {
@@ -26,7 +46,8 @@ public class TaskControllerUnitTest
 	{
 		var mappingConfig = new MapperConfiguration(x => x.AddMaps(Assembly.GetAssembly(typeof(TaskController))));
 		_mapper = new Mapper(mappingConfig);
-		
+		_taskService.Setup(service => service.GetAll(It.IsAny<TaskFilter>(), It.IsAny<Paggination>()))
+			.Returns(new PagedResponse<TaskEntityDto>());
 		
 		_taskController = new TaskController(_taskService.Object, _mapper);
 	}
